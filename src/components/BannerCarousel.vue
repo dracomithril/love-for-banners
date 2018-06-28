dd94cc69cc2c6d57ddfcdbc62f5fc34c4c00be3c
Michal Grezel <michal.grezel@grandparade.co.uk>
2018-06-28 19:22:49 +0200

    active buttons

--- src/components/BannerCarousel.vue
+++ src/components/BannerCarousel.vue
@@ -1,29 +1,48 @@
 <template>
-    <div>
-        <slick ref="slick" :options="slickOptions" class="banner-carousel">
-            <a href="https://img00.deviantart.net/e88c/i/2013/072/9/b/space_frontier__heimdall_by_nathanblackwolf-d5xvzxg.jpg">
-                <img class="banner-image" data-lazy="https://img00.deviantart.net/e88c/i/2013/072/9/b/space_frontier__heimdall_by_nathanblackwolf-d5xvzxg.jpg" alt="">
-            </a>
-            <a href="https://d1w9csuen3k837.cloudfront.net/Pictures/1280x720/9/9/2/132992_shutterstock_576050593.jpg">
-                <img class="banner-image" data-lazy="https://d1w9csuen3k837.cloudfront.net/Pictures/1280x720/9/9/2/132992_shutterstock_576050593.jpg" alt="">
-            </a>
+    <div class="banners">
+        <slick ref="slick" :options="slickOptions" class="banner-carousel" @beforeChange="handleBeforeChange" >
+                <a v-for="item in images"
+                   :href="item.src"
+                   :key="item.id">
+                    <img class="banner-image"
+                         :data-lazy="item.src"
+                         alt="">
+                </a>
         </slick>
+        <div ref="buttons" class="banner-buttons">
+            <button data-id="0" class="banner-button" v-on:click="setPosition(0)">banner1</button>
+            <button data-id="1" class="banner-button" v-on:click="setPosition(1)">banner2</button>
+        </div>
     </div>
 </template>
 <script>
 import Slick from 'vue-slick';
+
 export default {
   name: 'BannerCarousel',
   components: { Slick },
   data() {
     return {
+      images: [
+        {
+          id: 0,
+          src:
+            'https://img00.deviantart.net/e88c/i/2013/072/9/b/space_frontier__heimdall_by_nathanblackwolf-d5xvzxg.jpg',
+        },
+        {
+          id: 1,
+          src:
+            'https://d1w9csuen3k837.cloudfront.net/Pictures/1280x720/9/9/2/132992_shutterstock_576050593.jpg',
+        },
+      ],
       slickOptions: {
         //options can be used from the plugin documentation
         slidesToShow: 1,
         infinite: true,
         accessibility: true,
         adaptiveHeight: false,
-        // arrows: true,
+        autoplay: true,
+        arrows: false,
         // dots: true,
         // draggable: true,
         edgeFriction: 0.3,
@@ -44,60 +63,58 @@ export default {
       // Helpful if you have to deal with v-for to update dynamic lists
       this.$refs.slick.reSlick();
     },
+    handleBeforeChange(slick, current) {
+      let buttons = Array.from(this.$refs.buttons.children);
+      buttons.forEach(elem => {
+        if (elem.attributes['data-id'].value === current.currentSlide.toString()) {
+          elem.classList.add('banner-button__active');
+        } else {
+          elem.classList.remove('banner-button__active');
+        }
+      });
+    },
+    setPosition(position) {
+      this.$refs.slick.goTo(position, false);
+    },
   },
 };
 </script>
 
 <style>
-.slick-prev:before,
-.slick-next:before {
-  font-size: 20px;
-  line-height: 1;
-  opacity: 0.75;
-  color: black;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+.slick-slider {
+  margin: 30px auto 0;
 }
-.slick-arrow {
-  background: red;
+.slick-slide img {
+  height: 100%;
+  width: 100%;
 }
-.slick-next:before {
-  content: '>';
+.slick-slider .slick-track,
+.slick-slider .slick-list {
+  height: 13vw;
+  width: 544px;
 }
-.slick-prev:before {
-  content: '<';
+</style>
+<style scoped>
+.banners {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
 
-.slick-next {
-  right: -25px;
-}
-.slick-prev {
-  left: -25px;
-}
-.slick-slider {
+.banner-buttons {
+  margin-top: 5px;
   width: 544px;
-  margin: 30px auto 50px;
+  display: flex;
+  height: 40px;
+  margin-left: auto;
+  margin-right: auto;
+  justify-content: space-around;
 }
-.slick-prev,
-.slick-next {
-  font-size: 0;
-  line-height: 0;
-  position: absolute;
-  top: 50%;
-  display: block;
-  width: 20px;
-  height: 20px;
-  padding: 0;
-  -webkit-transform: translate(0, -50%);
-  -ms-transform: translate(0, -50%);
-  transform: translate(0, -50%);
-  cursor: pointer;
-  color: transparent;
-  border: none;
-  outline: none;
-  background: transparent;
+.banner-buttons .banner-button {
+  border: 1px solid blue;
+  width: 100%;
 }
-.banner-image {
-  width: 544px;
+.banner-buttons .banner-button__active {
+  border-top: 2px solid yellow;
 }
 </style>
